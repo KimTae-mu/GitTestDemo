6f522c71042a7038a536a9fbcd4c73fa06bedaa6
穆国超 <muguochao@qijiayoudao.com>
2018-08-23 11:54:21 +0800

    test --mgc_test

--- Test.java
+++ Test.java
@@ -2,5 +2,8 @@ public class Test{
 	public static void main(String args[]){
 		System.out.println("hello");
 		System.out.println("world22");
+    int a = 0;
+    int b=a;
+    System.out.println("a:"+a);
 	}
 }
