453b79e8ade169f04ea0ce0ac6c6285707db7469
Alva丶沐 <1160846772@qq.com>
2018-08-23 11:56:25 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,8 +2,8 @@ public class Test{
 	public static void main(String args[]){
 		System.out.println("hello");
 		System.out.println("world22");
-    int a = 0;
-    int b=a;
+		int c=100;
+    int a = c;
     System.out.println("a:"+a);
 	}
 }
